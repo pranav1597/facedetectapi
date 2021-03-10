0a21bc84da632a34ed9d3eb8547cfcb723a81181
pranav1597 <78584781+pranav1597@users.noreply.github.com>
2021-03-10 20:28:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profileid');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user : 'postgres',
+		password: '',
+		database: 'detectdb'
+	}
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req , res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req , res) => { profile.handleProfileId(req, res, db) })
+app.put('/profile/image', (req , res) => { image.handleImage(req, res, db) })
+app.post('/profile/imageurl', (req , res) => { image.handleApiCall(req, res) })
+
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
+})
