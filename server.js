08946a562e34cc4a3a73323b39119252e9ee32d2
pranav1597 <78584781+pranav1597@users.noreply.github.com>
2021-03-11 18:27:37 +0530

    knex host change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: ' postgresql-fluffy-43114',
 		user : 'postgres',
 		password: '',
 		database: 'detectdb'
