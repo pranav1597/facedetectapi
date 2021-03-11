9d7ea2d985f57476c937d0f3df773706f7cb9d99
pranav1597 <78584781+pranav1597@users.noreply.github.com>
2021-03-11 18:40:25 +0530

    updating db

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: ' postgresql-fluffy-43114',
-		user : 'postgres',
-		password: '',
-		database: 'detectdb'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
